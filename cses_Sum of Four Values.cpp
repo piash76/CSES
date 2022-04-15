6b87355021da4b88fe96b3fd0390434fbba32abb
piash76 <48187014+piash76@users.noreply.github.com>
2022-04-15 16:38:32 +0600

    Add files via upload

---
+++ cses_Sum of Four Values.cpp
@@ -0,0 +1,102 @@
+
+///Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+
+#define ll long long
+#define pll  pair<ll,ll>
+
+#define ff    first
+#define ss    second
+
+
+#define  pb  push_back
+
+#define cy  cout<<"YES"<<endl
+#define cn   cout<<"NO"<<endl
+
+
+using namespace std;
+
+const ll inf=1e15;
+const int mod=1e9+7;
+
+
+const int N=500005;
+pair<ll,ll> a[N];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    ll n,x;cin>>n>>x;
+
+    for(ll i=0;i<n;i++)
+    {
+        ll val;cin>>val;
+        a[i].first=val;
+        a[i].second=i;
+    }
+    sort(a,a+n);
+
+    ll id1=-1,id2=-1,id3=-1,id4=-1;
+
+
+
+
+    for(ll k=0;k<n;k++)
+    {
+        ll sm=a[k].first;
+
+        bool f=false;
+
+        for(ll l=k+1;l<n;l++)
+        {
+
+          ll sm2=a[l].first;
+          bool f2=false;
+
+
+            ll i=l+1,j=n-1;
+
+            while(i<j)
+            {
+                if(a[i].first+a[j].first==x-sm-sm2)
+                {
+                    id1=a[i].second,id2=a[j].second,id3=a[l].second,id4=a[k].second;
+                    f2=true;break;
+
+                }
+                else if((a[i].first+a[j].first)>x-sm-sm2) j--;
+                else i++;
+
+            }
+            if(f2)
+            {
+                f=true;
+                break;
+            }
+
+        }
+
+        if(f) break;
+
+    }
+
+
+
+    if(id1!=-1) cout<<id1+1<<" "<<id2+1<<" "<<id3+1<<" "<<id4+1<<endl;
+    else cout<<"IMPOSSIBLE"<<endl;
+
+
+
+    return 0;
+
+}
+
+
+
+
+
+
+
